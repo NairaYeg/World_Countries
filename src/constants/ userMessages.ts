22e8709af007743562704b3bbc52383cfa74ab5b
nyeghiazaryan <nyeghiazaryan@zscaler.com>
2023-05-12 19:39:50 +0400

    feat(constants): added Region andand userMessages constants

---
+++ src/constants/ userMessages.ts
@@ -0,0 +1,5 @@
+export const USER_MESSAGES = {
+    apiErrorMessage:   "An error occurred while fetching countries. Please try again later.",
+    noResultsMessage: "No results found.",
+  };
+  
---
+++ src/constants/regions.ts
@@ -0,0 +1,10 @@
+export enum Region {
+    Africa = "Africa",
+    Europe = "Europe",
+    Asia = "Asia",
+    Oceania = "Oceania",
+    Australia = "Australia",
+    NorthAmerica = "North America",
+    SouthAmerica = "South America",
+  }
+  
